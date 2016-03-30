b9383570065df18015c66488c6a05dd21efd3700
travelmac <cr1msaun@gmail.com>
2016-03-30 11:28:50 +0300

    print page html, php functions helper

---
+++ functions.php
@@ -0,0 +1,91 @@
+<?
+CModule::IncludeModule('iblock');
+$selected_date = $_GET['date'] ? $_GET['date'] : date('Y.m.d');
+
+// по месяцу в обе стороны
+function getDates() {
+	$dates = [];
+
+	for ($i = 0; $i < 31; $i++) {
+		array_push($dates, date('Y.m.d', strtotime('+' . $i . 'days')));
+	}
+
+	for ($i = 1; $i < 30; $i++) {
+		array_unshift($dates, date('Y.m.d', strtotime('-' . $i . 'days')));
+	}
+
+	return $dates;
+}
+
+function getScheduleHours() {
+	$hours = [];
+	$startHour = 5;
+
+	for ($startHour; $startHour <= 28; $startHour++) {
+		if ($startHour >= 24) 
+			$hour = $startHour - 24;
+		else
+			$hour = $startHour;
+
+		$hours[] = $hour . ':00';
+	}
+
+	return $hours;
+}
+
+function getMovies() {
+	$movies = [];
+
+	$moviesRes = CIBlockElement::GetList(
+		array('ID' => 'DESC'),
+		array(
+			'IBLOCK_ID' => MOVIES_IBLOCK_ID,
+			'ACTIVE' => 'Y',
+			'>DATE_CREATE' => ConvertTimeStamp(strtotime('-62 days'), "FULL")
+		)	
+	);
+
+	while ($movie = $moviesRes->GetNext())
+		$movies[] = $movie;
+
+	return $movies;
+}
+
+function getHalls() {
+	$halls = [];
+
+	$hallsRes = CIBlockElement::GetList(
+		array('ID' => 'ASC'),
+		array(
+			'IBLOCK_ID' => 6,
+			'ACTIVE' => 'Y'
+		)	
+	);
+
+	while ($hall = $hallsRes->GetNext())
+		$halls[] = $hall;
+
+	return $halls;
+}
+
+function getShowtimes($date, $hallId) {
+	$showtimes = [];
+
+	$showtimesRes = CIBlockElement::GetProperty(6, $hallId, array(), array("CODE" => "SHOWTIMES"));
+
+    while ($showtime = $showtimesRes->GetNext())
+    {	
+    	$showtimeDate = date('Y.m.d', strtotime($showtime['VALUE']));
+    	
+    	if ($showtimeDate != $date) continue;
+
+    	list($name, $duration, $break, $format) = explode('//', $showtime['DESCRIPTION']);
+
+    	$start_time = date('H:i', strtotime($showtime['VALUE']));
+    	$end_time = date('H:i', strtotime($showtime['VALUE'] . ' +' . $duration . 'minutes'));
+
+        $showtimes[] = array('NAME' => $name, 'START_TIME' => $start_time, 'END_TIME' => $end_time, 'DURATION' => $duration, 'BREAK' => $break, 'FORMAT' => $format);
+    }
+
+    return $showtimes;
+}
